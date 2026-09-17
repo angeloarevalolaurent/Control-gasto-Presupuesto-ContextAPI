ee7b2bedb63ed3105d9d4b506b49f2f2cd86c4bc
Angelo Arevalo <arevaloangelomarcial@gmail.com>
2026-09-17 10:40:09 -0500

    Mostrando el Modal cuando agregamos un nuevo gasto

--- src/reducers/budgett-reducer.ts
+++ src/reducers/budgett-reducer.ts
@@ -1,15 +1,18 @@
 export type BudgetActions =
-    {type: 'add-budget', payload:{ budget: number}}
+    {type: 'add-budget', payload:{ budget: number}} |
+    {type: 'show-modal'}
 
 
 
     export type BudgetState = {
         budget: number
+        modal: boolean
     }
 
 
     export const initialState : BudgetState ={
-        budget : 0
+        budget : 0,
+        modal: false
     }
 
 
@@ -23,6 +26,14 @@ export type BudgetActions =
             return{
                 ...state,
                 budget: action.payload.budget
+            }
+        }
+
+        if (action.type === 'show-modal') {
+            
+            return{
+                ...state,
+                modal: true
             }
         }
 
