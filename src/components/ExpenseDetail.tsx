64c9b7d3f73f172b7368c53a507ec07ed49638e4
Angelo Arevalo <arevaloangelomarcial@gmail.com>
2026-09-22 17:55:09 -0500

    Mostramos los gstos - Agregamos formatDate(helpers)

--- src/components/ExpenseDetail.tsx
+++ src/components/ExpenseDetail.tsx
@@ -1,4 +1,6 @@
+import { formatDate } from "../helpers"
 import type { Expense } from "../types"
+import { AmountDisplay } from "./AmountDisplay"
 
 type ExpenseDetailProps = {
     expense: Expense
@@ -6,6 +8,20 @@ type ExpenseDetailProps = {
 
 export const ExpenseDetail = ({expense}: ExpenseDetailProps) => {
   return (
-    <div>ExpenseDetail</div>
+    <div className="bg-white border-2 border-gray-200 p-10 shadow-lg w-full flex gap-5 items-center">
+        
+        <div>
+
+
+        </div>
+        <div>
+            <p>{expense.expenseName}</p>
+            <p className="text-slate-600 text-sm">{formatDate(expense.date!.toString())}</p>
+        </div>
+        
+        <AmountDisplay
+            amount={expense.amount}
+        />
+    </div>
   )
 }
