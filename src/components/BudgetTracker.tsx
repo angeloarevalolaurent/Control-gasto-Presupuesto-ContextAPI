aa2bb198b3ecb36a5a4c08f92f6b409c8301fa48
Angelo Arevalo <arevaloangelomarcial@gmail.com>
2026-09-24 16:48:18 -0500

    Creamos una accion reset-app para reiniciar la app

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -7,7 +7,7 @@ import { AmountDisplay } from './AmountDisplay';
 
 
 export const BudgetTracker = () => {
-    const {state, totalExpenses, remainingBudget} = useBudget()
+    const {state,dispatch ,totalExpenses, remainingBudget} = useBudget()
     
     const procentage = +((totalExpenses / state.budget)*100).toFixed(2)
    
@@ -30,6 +30,7 @@ export const BudgetTracker = () => {
 
         <div className="flex flex-col justify-center items-center gap-8">
             <button
+                    onClick={()=> dispatch({type:'reset-app'})}
                     type="button"
                     className="bg-pink-600 w-full p-2 text-white uppercase font-bold rounded-lg"
             >
--- src/reducers/budgett-reducer.ts
+++ src/reducers/budgett-reducer.ts
@@ -8,7 +8,8 @@ export type BudgetActions =
     {type:'add-expense', payload:{expense: DraftExpense}}|
     {type:'delete-expense', payload:{id: Expense['id']}} |
     {type:'get-expense-by-id', payload:{id: Expense['id']}}|
-    {type:'update-expense', payload:{expense:Expense}}
+    {type:'update-expense', payload:{expense:Expense}} |
+    {type: 'reset-app'}
 
 
 
@@ -114,6 +115,15 @@ export type BudgetActions =
                 expenses: state.expenses.map(expense  => expense.id === action.payload.expense.id
                      ? action.payload.expense : expense),
                 modal: false    
+            }
+        }
+
+        if (action.type === 'reset-app') {
+            
+            return{
+                ...state,
+                expenses: [],
+                budget: 0
             }
         }
         return state
