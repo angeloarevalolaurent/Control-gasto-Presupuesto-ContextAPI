4ff89327bc014c6a6655b88cb229f03ae4600392
Angelo Arevalo <arevaloangelomarcial@gmail.com>
2026-09-24 10:36:30 -0500

    Colocando gastos en localStorage

--- src/reducers/budgett-reducer.ts
+++ src/reducers/budgett-reducer.ts
@@ -20,12 +20,26 @@ export type BudgetActions =
     }
 
 
+    const initialBudget = () : number => {
+        const localStorageBudget = localStorage.getItem('budget')
+        return localStorageBudget ? +localStorageBudget : 0
+        }
+
+
+    const localStorageExpense = () : Expense[] => {
+        const localStorageExpenses = localStorage.getItem('expenses')
+        return localStorageExpenses ? JSON.parse(localStorageExpenses) :[]
+    }
+    
+    
     export const initialState : BudgetState ={
-        budget : 0,
+        budget : initialBudget(),
         modal: false,
-        expenses: [],
+        expenses: localStorageExpense(),
         editingId:''
     }
+
+   
 
     const createExpense = (draftExpense: DraftExpense): Expense =>{
 
