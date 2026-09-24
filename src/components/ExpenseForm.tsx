ae8c300d18ac56b6ba1fc6d04de16fadd1f62e79
Angelo Arevalo <arevaloangelomarcial@gmail.com>
2026-09-24 10:19:27 -0500

    Mostrar de forma condicional si es un gasto nuevo o edicion

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -73,7 +73,7 @@ export const ExpenseForm = () => {
     <form  className="space-y-5" onSubmit={handleSubmit}>
 
         <legend className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2">
-            Nuevo Gasto
+           {state.editingId ? 'GuardarCambios' :'Nuevo gasto'  }
         </legend>
 
         {error && <ErrorMessage>{error}</ErrorMessage> }
@@ -157,7 +157,7 @@ export const ExpenseForm = () => {
             type="submit" 
             className="bg-blue-600 cursor-pointer w-full p-2 text-white uppercase font-bold 
                          rounded-lg" 
-            value={'Registrar Gasto'}
+            value={state.editingId ? 'GuardarCambios' :'Nuevo gasto'  }
           />
     </form>
 
--- src/context/BudgetContext.tsx
+++ src/context/BudgetContext.tsx
@@ -23,7 +23,7 @@ export const BudgetProvider = ({children}:BudgetProviderProps) => {
                 state,
                 dispatch
             }}
-        
+         
         >
             {children}
         </BudgetContext.Provider>
